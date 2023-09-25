3feec16fe312c4916d66a6f12a5013e60554df54
Ingewar <kovtunihor@gmail.com>
2023-09-25 20:30:44 +0300

    setup HandController

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public Card[] cards;
+    [SerializeField] private Transform minPos, maxPos;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetCardPositions() { }
+}
